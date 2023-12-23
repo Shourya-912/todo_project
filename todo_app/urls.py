b72b03d96905cc2238a8f1e78eb82d324857c3ab
Shourya-912 <shourchourasia.912@gmail.com>
2023-12-23 17:46:13 +0530

    git action confirmation

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,12 @@
 # todo_app/urls.py
 
 from django.urls import path
-from .views import TodoListCreateView, TodoRetrieveUpdateDestroyView
+from .views import (
+    TodoListCreateView,
+    TodoRetrieveUpdateDestroyView,
+    TagListView,
+    TagDetailView,
+)
 
 urlpatterns = [
     path("todos/", TodoListCreateView.as_view(), name="todo-list-create"),
@@ -10,4 +15,6 @@ urlpatterns = [
         TodoRetrieveUpdateDestroyView.as_view(),
         name="todo-retrieve-update-destroy",
     ),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,15 +1,52 @@
-# todo_app/views.py
-
 from rest_framework import generics
-from .models import Todo
+from django.shortcuts import render
+from .models import Tag, Todo
 from .serializers import TodoSerializer
+from django.views.generic import ListView, DetailView
 
 
 class TodoListCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+    def list(self, request, *args, **kwargs):
+        todos = self.get_queryset()
+        return render(request, "todo_app/todo_list.html", {"todos": todos})
+
+    def create(self, request, *args, **kwargs):
+        # Your existing logic for creating a Todo
+        return super().create(request, *args, **kwargs)
+
 
 class TodoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        todo = self.get_object()
+        return render(request, "todo_app/todo_detail.html", {"todo": todo})
+
+    def update(self, request, *args, **kwargs):
+        # Your existing logic for updating a Todo
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        # Your existing logic for deleting a Todo
+        return super().destroy(request, *args, **kwargs)
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "todo_app/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagDetailView(DetailView):
+    model = Tag
+    template_name = "todo_app/tag_detail.html"
+    context_object_name = "tag"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todos"] = Todo.objects.filter(tags=self.object)
+        return context
